2dc29c6f041a0ab9e1281ef976e2e2dc21805e87
alphaCentavra <alpha_net@tut.by>
2023-02-14 20:40:03 +0300

    Initial commit

---
+++ example1/src/main/java/SqlSampler.java
@@ -0,0 +1,44 @@
+import java.sql.*;
+
+public class SqlSampler {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+                System.out.println("No sqlite driver");
+                return;
+        }
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            connection =
+                DriverManager.getConnection("jdbc:sqlite:C:\\Users\\user\\Downloads\\sqlite-tools-win32-x86-3330000\\sqlite-tools-win32-x86-3330000\\mydb2.db");
+            statement = connection.createStatement();
+            //если множество ResultSet
+           // statement.execute("");
+            //один ResultSet
+            ResultSet rs = statement.executeQuery("SELECT * FROM students WHERE average_grade>7");
+            while (rs.next()) {
+                System.out.println(rs.getString("name"));
+            }
+            //запрос без возвращения результатов
+           // statement.executeUpdate("INSERT INTO students VALUES()");
+            statement.close();
+            connection.close();
+        }  catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println("Can't close the connection");
+            }
+        }
+    }
+}
---
+++ example2/src/main/java/SqlSampler.java
@@ -0,0 +1,74 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SqlSampler {
+
+
+    public static class StudentNames {
+        private String name;
+        private String lastName;
+
+        public StudentNames(String name, String lastName) {
+            this.name = name;
+            this.lastName = lastName;
+        }
+    }
+
+    private static Map<Double, List<StudentNames>> mapGrades;
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+                System.out.println("No sqlite driver");
+                return;
+        }
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            connection =
+                DriverManager.getConnection("jdbc:sqlite:C:\\Users\\user\\Downloads\\sqlite-tools-win32-x86-3330000\\sqlite-tools-win32-x86-3330000\\mydb2.db");
+            statement = connection.createStatement();
+            //если множество ResultSet
+           // statement.execute("");
+            //один ResultSet
+            final ResultSet rs = statement.executeQuery("SELECT name,last_name,average_grade FROM students");
+            mapGrades = new HashMap<Double, List<StudentNames>>();
+            while (rs.next()) {
+                Double grade = rs.getDouble("average_grade");
+                if (mapGrades.get(grade) == null) {
+                    mapGrades.put(grade, new ArrayList<StudentNames>());
+                }
+                mapGrades.get(grade).add(new StudentNames(rs.getString(1), rs.getString(2)));
+                //System.out.println(rs.getString("name"));
+            }
+            for (List<StudentNames> lst : mapGrades.values()) {
+                if (lst.size() > 1) {
+                    for (StudentNames names : lst) {
+                        System.out.println(names.name + " " + names.lastName);
+                    }
+                }
+            }
+            //запрос без возвращения результатов
+            //statement.executeUpdate("INSERT INTO students VALUES(5, 'Alex', 'Str', 12, 7.2)");
+            //statement.close();
+            //connection.close();
+        }  catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println("Can't close the connection");
+            }
+        }
+    }
+}
